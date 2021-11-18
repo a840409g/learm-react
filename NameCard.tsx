be65c93fef03e020978ac64f7e41fdbc17ad760e
joe0409_chen <joe0409_chen@mail.chungyo.net>
2021-11-18 17:59:33 +0800

    chore: 紀錄

---
+++ NameCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface NameCard {
+    name:string,
+    age:number,
+    gender: string
+}
+function NameCard ({name}:NameCard){
+
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types'; // 引入props type checker
 import './App.css';
+/*
+    渲染函式的第一個參數是props
+    因為是物件所以可以解構, 且透過物件預設可以預設props value
+*/
+function NameCard ({ name, age, gender = '男' }) {
+    return (
+        <div className = "card-name">
+            我是: { name }, 今年: { age }, 是{ gender }姓
+        </div>
+    );
+}
+// props的型別檢查
+NameCard.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    gender: PropTypes.string
+};
 
 function App () {
     return (
-        <div className = "App">
-            <header className = "App-header">
-                <img src = { logo } className = "App-logo" alt = "logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className = "App-link"
-                    href = "https://reactjs.org"
-                    target = "_blank"
-                    rel = "noopener noreferrer"
-                >
-                    Learn React
-                    {a}
-                </a>
-            </header>
+        <div>
+            {/* 組件引入props */}
+            <NameCard
+                name = "JJJ"
+                age = { 12 }
+            />
         </div>
     );
 }
